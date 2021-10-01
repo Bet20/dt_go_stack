c499b4195f2f30da7b0e16e76a183624afb8d048
Bet20 <dcprodutionspt@gmail.com>
2021-10-01 17:12:18 +0100

    Initial Commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module dt_go_stack
+
+go 1.16
---
+++ main.go
@@ -0,0 +1,35 @@
+package main
+
+type stack struct {
+	nodes []interface{}
+	size  int
+}
+
+func (s *stack) Push(value interface{}) {
+	if len(s.nodes) < s.size {
+		s.nodes = append(s.nodes, value)
+	}
+}
+
+func (s stack) Peek() interface{} {
+	return s.nodes[len(s.nodes)-1]
+}
+
+func (s *stack) Pop() interface{} {
+
+	if s.IsEmpty() {
+		return nil
+	}
+
+	ret := s.nodes[len(s.nodes)-1]
+	s.nodes = s.nodes[0 : len(s.nodes)-1]
+	return ret
+}
+
+func (s *stack) IsEmpty() bool {
+	return len(s.nodes) < 1
+}
+
+func (s *stack) IsFull() bool {
+	return s.size == len(s.nodes)
+}
